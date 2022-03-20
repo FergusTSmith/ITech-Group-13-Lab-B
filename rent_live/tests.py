731e6765835d9abb359948010a60b2e9934ae870
vincent972123 <392025574@qq.com>
2022-03-20 05:59:35 +0000

    update tests.py

--- rent_live/tests.py
+++ rent_live/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 from rent_live.models import *
 from django.urls import reverse
+from django import template
+
+
 # Create your tests here.
 
+# Test model
 class CategoryMethodTests(TestCase):
     def test_category(self):
         category = Category(name='test', description='abcd')
@@ -31,8 +35,14 @@ class LettingAgentTests(TestCase):
         self.assertEqual((letting_agent.email != 'null'), True)
         
         
-
+# Test view
 class IndexViewTests(TestCase):
-    def test_index_view_with_no_categories(self):
+    def test_index_view(self):
         response = self.client.get(reverse('rent_live:index'))
         self.assertEqual(response.status_code, 200)
+    
+    def test_visit_a_nonexistent_category(self):
+        url = reverse('rent_live:category')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
